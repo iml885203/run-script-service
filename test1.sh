3c5996abe3d2ac053b27ea1a7b63b4ef75f1cedc
Logan <iml885203@gmail.com>
2025-08-06 09:25:54 +0800

    chore: remove unnecessary test scripts and update gitignore
    - Remove test1.sh, test2.sh, test_integration.sh, upgrade.sh
    - Add test*.sh pattern to gitignore to prevent future commits
    - Clean up repository from development artifacts

--- test1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Script 1 running at $(date)"
-sleep 1
-echo "Script 1 completed"
--- test2.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Script 2 starting..."
-sleep 2
-echo "Script 2 finished at $(date)"
